f8bc53bdf612a00f81750eaad3162c0d5d6b63fb
Andrew Schutte <47733373+almostBurtMacklin@users.noreply.github.com>
2019-10-23 17:13:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 import dash
-import flask
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
 external_stylesheets = [ dbc.themes.BOOTSTRAP, 'https://codepen.io/almostburtmacklin/pen/QWWKEJb.css']
-server = flask.Flask(__name__)
-@server.route('/apps')
-def index():
-    return 'Hello Flask app'
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
-                server = server)
-
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 app.config.suppress_callback_exceptions = True
