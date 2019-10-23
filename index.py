a512476e2e8d4daa7467fef9cefbd66829b2e94f
Andrew Schutte <47733373+almostBurtMacklin@users.noreply.github.com>
2019-10-23 17:23:50 -0500

    Update index.py

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 from app import app
+from app import server
 from apps import pitch, first
 
 
