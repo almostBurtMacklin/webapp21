1c397de39b76c9116929a4315ba376174168451b
Andrew Schutte <47733373+almostBurtMacklin@users.noreply.github.com>
2019-10-23 17:43:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import dash_bootstrap_components as dbc
 
 external_stylesheets = [ dbc.themes.BOOTSTRAP, 'https://codepen.io/almostburtmacklin/pen/QWWKEJb.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"}])
 server = app.server
 app.config.suppress_callback_exceptions = True
