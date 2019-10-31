edeb35b1f675671c50e6e0f5f5dcbf64623050bb
Andrew Schutte <47733373+almostBurtMacklin@users.noreply.github.com>
2019-10-31 13:14:59 -0500

    Add files via upload

--- index.py
+++ index.py
@@ -9,10 +9,34 @@ from apps import pitch
 from apps import first
 from apps import bal
 from apps import nyy
-from apps import tor
 from apps import tbr
 from apps import bos
-
+from apps import tor
+from apps import mins
+from apps import cle
+from apps import chw
+from apps import kcr
+from apps import det
+from apps import hou
+from apps import oak
+from apps import tex
+from apps import laa
+from apps import sea
+from apps import atl
+from apps import wsn
+from apps import phi
+from apps import nym
+from apps import mia
+from apps import stl
+from apps import mil
+from apps import chc
+from apps import cin
+from apps import pit
+from apps import lad
+from apps import sdp
+from apps import sfg
+from apps import ari
+from apps import col
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
@@ -27,16 +51,66 @@ def display_page(pathname):
         return first.layout
     elif pathname == '/pitch':
         return pitch.layout
-    elif pathname == '/apps/bal':
+    elif pathname == '/bal':
         return bal.layout
-    elif pathname == '/apps/nyy':
+    elif pathname == '/nyy':
         return nyy.layout
-    elif pathname == '/apps/tbr':
+    elif pathname == '/tbr':
         return tbr.layout
-    elif pathname == '/apps/bos':
+    elif pathname == '/bos':
         return bos.layout
-    elif pathname == '/apps/tor':
+    elif pathname == '/tor':
         return tor.layout
+    elif pathname == '/mins':
+        return mins.layout
+    elif pathname == '/cle':
+        return cle.layout
+    elif pathname == '/chw':
+        return chw.layout
+    elif pathname == '/kcr':
+        return kcr.layout
+    elif pathname == '/det':
+        return det.layout
+    elif pathname == '/hou':
+        return hou.layout
+    elif pathname == '/oak':
+        return oak.layout
+    elif pathname == '/tex':
+        return tex.layout
+    elif pathname == '/laa':
+        return laa.layout
+    elif pathname == '/sea':
+        return sea.layout
+    elif pathname == '/atl':
+        return atl.layout
+    elif pathname == '/wsn':
+        return wsn.layout
+    elif pathname == '/phi':
+        return phi.layout
+    elif pathname == '/nym':
+        return nym.layout
+    elif pathname == '/mia':
+        return mia.layout
+    elif pathname == '/stl':
+        return stl.layout
+    elif pathname == '/mil':
+        return mil.layout
+    elif pathname == '/chc':
+        return chc.layout
+    elif pathname == '/cin':
+        return cin.layout
+    elif pathname == '/pit':
+        return pit.layout
+    elif pathname == '/lad':
+        return lad.layout
+    elif pathname == '/sdp':
+        return sdp.layout
+    elif pathname == '/sfg':
+        return sfg.layout
+    elif pathname == '/ari':
+        return ari.layout
+    elif pathname == '/col':
+        return col.layout
     else:
         return first.layout
 
