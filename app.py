25463a5f7400c03c7c11dda0b9a5eaac1c1a52c4
Andrew Schutte <47733373+almostBurtMacklin@users.noreply.github.com>
2019-11-02 18:27:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,11 @@ from scout_apm.flask import ScoutApm
 
 external_stylesheets = [ dbc.themes.BOOTSTRAP, 'https://codepen.io/almostburtmacklin/pen/QWWKEJb.css']
 
-ScoutApm(app)
+
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1"}])
+ScoutApm(app)
 server = app.server
 app.config.suppress_callback_exceptions = True
-
-
-
-
-app.config["SCOUT_MONITOR"] = True
-app.config["SCOUT_KEY"] = "DLHussA7KhTv6t1G8CmG"
 app.config["SCOUT_NAME"] = "Baseball Webb App"
