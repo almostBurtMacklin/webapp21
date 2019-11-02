31752737ab4f31c471dae80d6b5a881302407e92
Andrew Schutte <47733373+almostBurtMacklin@users.noreply.github.com>
2019-11-02 18:23:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from scout_apm.flask import ScoutApm
 
 external_stylesheets = [ dbc.themes.BOOTSTRAP, 'https://codepen.io/almostburtmacklin/pen/QWWKEJb.css']
 
+ScoutApm(app)
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1"}])
 server = app.server
@@ -13,7 +15,6 @@ app.config.suppress_callback_exceptions = True
 
 
 
-ScoutApm(app)
 
 app.config["SCOUT_MONITOR"] = True
 app.config["SCOUT_KEY"] = "DLHussA7KhTv6t1G8CmG"
