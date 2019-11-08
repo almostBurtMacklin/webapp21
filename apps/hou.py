970b7deadfa2f9caab6df99e86010550ac62ab07
Andrew Schutte <47733373+almostBurtMacklin@users.noreply.github.com>
2019-11-08 12:09:55 -0600

    Update hou.py

--- apps/hou.py
+++ apps/hou.py
@@ -16,15 +16,15 @@ import dash_bootstrap_components as dbc
 df = pd.read_csv('data/hou.csv', dtype={'b_count': 'category', 's_count' : 'category', 'pitcher_id' : 'category', 'pitch_type' : 'category', 'stand': 'category' })
 
 teamColor = [[0, "#fff"],
-                [0.25, "#f4d4d1"],
-                [0.45, "#f5a9a4"],
-                [0.65, "#f17e76"],
-                [0.85, "#ec5349"],
-                [1, "#e8291c"]]
+                [0.25, "#fbe2d2"],
+                [0.45, "#f7c5a5"],
+                [0.65, "#f3a878"],
+                [0.85, "#ef8b4b"],
+                [1, "#eb6e1f"]]
 
-darker = '#1d2d5c'
-lighter = '#134a8e'
-bright = '#e8291c'
+darker = '#002d62'
+lighter = '#eb6e1f'
+bright = '#f4911e'
 
 features = df.pitcher_id.unique()
 pitches = df.pitch_type.unique()
@@ -42,7 +42,7 @@ strike6 = [{'label' : '0', 'value' : '0.0'},            #increase num
 batter6 = [{'label' : 'Right', 'value' : 'R'},          #increase num
          {'label' : 'Left', 'value' : 'L'}]
 
-trace_1 = go.Histogram2d(x = df.px, y = df.pz, colorscale=teamColor)
+trace_1 = go.Histogram2d()
 
 layouts = go.Layout(height = 600,
                    width = 600)
@@ -89,10 +89,10 @@ layout = html.Div([
                         #html.Img(src='data:oriole/jpg;base64,{}'.format(encoded_image.decode()),style ={'width': '99%'}),
                         html.H1("Houston Astros Match Up Chart - Pitcher Tendencies",
                          style = {#'backgroundColor' : '#512888',
-                                 'color': '#e8291c',
+                                 'color': lighter,
                                   'text-align' : 'center',
                                   'height': '50px',
-                                  'text-shadow' : '-1px -1px 0 #c4ced3, 1px -1px 0 #c4ced3, -1px 1px 0 #c4ced3, 1px 1px 0 #c4ced3'}),
+                                  'text-shadow' : '-1px -1px 0 #eb6e1f, 1px -1px 0 #eb6e1f, -1px 1px 0 #eb6e1f, 1px 1px 0 #eb6e1f'}),
                         
                         html.P([
                             html.P("Pitcher", style={'color' : 'white'}),
@@ -163,7 +163,7 @@ layout = html.Div([
                         style_data_conditional=[ {
                                 'if': {'column_id': str(x), 'filter_query': '{{{0}}} > 25 && {{{0}}} < 100'.format(x)},
                                 'color': 'white',
-                                'backgroundColor' : darker
+                                'backgroundColor' : lighter
                             } for x in fin.columns.to_list()
                         ], style_table={'width': '95%'}),
                         
@@ -176,11 +176,11 @@ layout = html.Div([
                         style_data_conditional=[ {
                                 'if': {'column_id': str(x), 'filter_query': '{{{0}}} > 25 && {{{0}}} < 100'.format(x)},
                                 'color': 'white',
-                                'backgroundColor' : darker
+                                'backgroundColor' : lighter
                             } for x in finR.columns.to_list()
                         ],
                         style_table={'width': '95%'})], className = "six columns")], className = "row")], style={
-                                                                    'backgroundColor' : lighter                   
+                                                                    'backgroundColor' : darker                   
                                                                     },className = "all")
                     
 ##                    html.Div([
